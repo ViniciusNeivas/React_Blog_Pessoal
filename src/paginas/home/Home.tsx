af57c2bea00a51d6c27a7b94ba2521ff9cee3e31
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-04-25 19:57:18 -0300

    React Dia!

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+
+function Home () {
+
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+          <img src="https://www.artmarketstudies.org/wp-content/uploads/2016/10/blogging.jpg" alt="Testes" className="img" />
+        </>
+
+    );
+
+}
+
+export default Home;
