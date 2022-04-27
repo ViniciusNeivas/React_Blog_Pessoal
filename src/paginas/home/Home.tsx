553e275caf78851a1a3a4ebc56b39262cc094faf
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-04-26 23:48:43 -0300

    React Dia 2 - Modelo Blog Pessoal Com  Materia UI, Grid, FlexBox,NavBar, Footer

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
 import './Home.css';
+import { Box } from '@mui/material';
 
-function Home () {
-
+function Home() {
     return (
         <>
-            <h1 className="titulo">Home</h1>
-          <img src="https://www.artmarketstudies.org/wp-content/uploads/2016/10/blogging.jpg" alt="Testes" className="img" />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja Bem Vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Expresse Aqui Os Seus Pensamentos E Opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
-
     );
-
 }
 
 export default Home;
